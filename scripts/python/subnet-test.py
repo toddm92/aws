d857adde37e44d83ffa28820e104b7a668640e6d
Todd <toddm92@gmail.com>
2014-10-23 22:08:46 -0700

    added the Frankfurt region

--- scripts/python/subnet-test.py
+++ scripts/python/subnet-test.py
@@ -16,7 +16,7 @@ import sys
 PROFILE = 'toddm92'  # (from your ~/.boto)
 REGIONS = ( 'us-east-1',  'eu-west-1',  'ap-northeast-1',
             'us-west-1', 'us-west-2', 'ap-southeast-1',
-            'ap-southeast-2', 'sa-east-1' )
+            'ap-southeast-2', 'sa-east-1', 'eu-central-1' )
 # **
 
 # Dummy varibles
